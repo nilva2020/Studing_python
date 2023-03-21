f531b6af51bd772e32d819923c4a89d3f363f3a0
nilva2020 <piresnilva@gmail.com>
2023-03-21 17:31:32 -0300

    create new file

---
+++ variaveis_atribuicoes.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+# Faça um programa que exiba seu nome na tela
+escreva = "Nilva_Pires"
+print(escreva)
+
+
+# In[8]:
+
+
+#Escreva um programa que exiba o resultado de 2a x 3b, em que: a vale3 d b vale 5
+a = 3
+b = 5
+
+
+# In[10]:
+
+
+print((2*a)*(3*b))
+
+
+# In[11]:
+
+
+#Escreve um programa que calcule a soma de tres variaveis e imprima na tela
+a = 44
+b = 33
+c = 76
+
+
+# In[12]:
+
+
+print(a+b+c)
+
+
+# In[19]:
+
+
+# Escreve um programa Calcula o aumento de salário
+salario = 3935
+reajuste = 15
+
+
+# In[20]:
+
+
+print(salario + (salario * reajuste / 100))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
